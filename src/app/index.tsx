2c6ea60a97b11e0ed8c2487533bf4c2509005e88
pain-arch <himulol123@gmail.com>
2024-12-03 17:48:09 +0600

    refactor App component to Home and simplify structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up inde.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text></Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Home
+
+const styles = StyleSheet.create({})
